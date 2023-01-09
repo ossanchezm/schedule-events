c7b263d3d31a56d1f2cc9f4c1982c11895a55637
Sebastian Sanchez <ossanchezm@gmail.com>
2023-01-09 13:23:06 -0600

    first commit

---
+++ pages/about-us.js
@@ -0,0 +1,57 @@
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1> About Us Page</h1>
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo.  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur.
+      </p>
+
+      <h2>consequuntur magni dolore </h2>
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo.  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur.
+      </p>
+      <ul className="about_us_list">
+        <li>
+        <h3>Consequuntur magni dolore </h3>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+            illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+            explicabo.  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+            odit aut fugit, sed quia consequuntur magni dolores ratione
+            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+            quia dolor sit amet, consectetur.
+          </p>
+
+        </li>
+        <li>
+        <h3>Sed ut perspiciatis unde </h3>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+            illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+            explicabo.  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+            odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+            quia dolor sit amet, consectetur.
+          </p>
+        </li>
+
+      </ul>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+    <footer>
+    <p> © Sebastian Sanchez - 2023, All rights reserved.</p>
+  </footer>
+)
